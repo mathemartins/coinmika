36350ea732592f5cd0a67f4a03231a73c03205d5
mathemartins <mathegeniuse@gmail.com>
2022-05-09 10:05:08 +0100

    urls mod

--- wallet/api/urls.py
+++ wallet/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from wallet.api.views import (
-    WalletCreate
+    WalletCreate, WalletRestore, ListWallets
 )
 
 urlpatterns = [
-    path('create/', WalletCreate.as_view(), name="create-wallet")
+    path('create/', WalletCreate.as_view(), name="create-wallet"),
+    path('restore/', WalletRestore.as_view(), name="restore-wallet"),
+    path('list/', ListWallets.as_view(), name="list-wallet"),
 ]
